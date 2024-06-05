6e772b918a1c0eba35ed5d0b16276e85b6a3f714
presstube <james.presstube@gmail.com>
2024-06-05 13:17:23 -0400

    adding 1 more func

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const cjs = createjs; // this will throw if createjs not already here
 // figure that out someday maybe
 
 const presstubeutils = {
-  distance(pt1, pt2) {
-    // Implementation for distance calculation
-    console.log("ptutils distance here!");
-  },
-  angle(pt1, pt2) {
-    // Implementation for angle calculation
-    console.log("ptutils angle here!");
-  },
-  fartypants(thePantsInWhichToFart) {
-    // Implementation for fartypants
-    console.log("ptutils fartypants here!");
+  springMoveTo({ subject, target, parent, spring = 0.1, friction = 0.8 }) {
+    let subjectPos = subject.localToLocal(0, 0, parent);
+    let targetPos = target.localToLocal(0, 0, parent);
+    if (!subject.vX) {
+      subject.vX = 0;
+    }
+    if (!subject.vY) {
+      subject.vY = 0;
+    }
+    subject.vX += (targetPos.x - subjectPos.x) * spring;
+    subject.vY += (targetPos.y - subjectPos.y) * spring;
+    subject.vX *= friction;
+    subject.vY *= friction;
+    subject.x += subject.vX;
+    subject.y += subject.vY;
   },
 
   makeTriangle(color, width, height) {
