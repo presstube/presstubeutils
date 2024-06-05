10def6473d400932210c47d0912039587c479907
presstube <james.presstube@gmail.com>
2024-06-05 10:57:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PTUtils
+
+A collection of handy functions used on Presstube projects by James Paterson. Many of them lean on CreateJS by Grant Skinner.
+
+## Installation
+
+```bash
+npm install presstubeutils
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const PTUtils = {
+  distance(pt1, pt2) {
+    // Implementation for distance calculation
+    console.log("ptutils distance here!");
+  },
+  angle(pt1, pt2) {
+    // Implementation for angle calculation
+    console.log("ptutils angle here!");
+  },
+};
+
+module.exports = PTUtils;
