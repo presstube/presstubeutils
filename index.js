0b52fc73d14d9a37459fefd8977fc962b4226188
presstube <james.presstube@gmail.com>
2024-06-05 11:07:49 -0400

    added fartypants test

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import "createjs";
+
+console.log("createjs: ", createjs);
+
 const PTUtils = {
   distance(pt1, pt2) {
     // Implementation for distance calculation
@@ -6,6 +10,10 @@ const PTUtils = {
   angle(pt1, pt2) {
     // Implementation for angle calculation
     console.log("ptutils angle here!");
+  },
+  fartypants(thePantsInWhichToFart) {
+    // Implementation for fartypants
+    console.log("ptutils fartypants here!");
   },
 };
 
